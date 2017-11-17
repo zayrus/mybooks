0e211ac19bb9f18b577861e0dfbec4e44b47ccdf
Ernest <zayrus@gmail.com>
2017-11-17 08:58:15 -0300

    fix(list books): select correct state of the book

--- src/Book.js
+++ src/Book.js
@@ -14,8 +14,8 @@ class Book extends Component {
         <div className="book-top">
           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
           <div className="book-shelf-changer">
-            <select value={"none"} onChange={(event) => {this.handleChange({id: book.id}, event.target.value)}}>
-              <option value="none" disabled>Move to...</option>
+            <select value={ book.shelf } onChange={(event) => {this.handleChange({id: book.id}, event.target.value)}}>
+              <option value="none" disabled>Move to... </option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
               <option value="read">Read</option>

4cabf81d0671882b50bee22cbbe35e439c5a1b60
Ernest <zayrus@gmail.com>
2017-11-20 11:20:57 -0300

    fix(book): manage how to display authors
    when a book has more than one author show names separeted by comma and books without author are
    showed as Anonymous

--- src/Book.js
+++ src/Book.js
@@ -9,12 +9,14 @@ class Book extends Component {
   render() {
     const { book } = this.props
     const image = book.imageLinks ? `url(${book.imageLinks.thumbnail})` : `url(http://scottishbooktrust.com/files/styles/book-cover-book-page/public/cover-not-available_212.png?itok=OazIlpdt)`
+    const authors = book.authors ? book.authors && book.authors.join(', ') : 'Anonymous'
+
     return (
       <div className="book">
         <div className="book-top">
           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: image }}></div>
           <div className="book-shelf-changer">
-            <select value={ book.shelf || `none`} onChange={(event) => {this.handleChange({id: book.id}, event.target.value)}}>
+            <select value={ book.shelf || `none`} onChange={(event) => { this.handleChange({id: book.id}, event.target.value) }}>
               <option value="none" disabled>Move to... </option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
@@ -23,8 +25,8 @@ class Book extends Component {
             </select>
           </div>
         </div>
-        <div className="book-title">{book.title}</div>
-        <div className="book-authors">{book.authors}</div>
+        <div className="book-title">{ book.title }</div>
+        <div className="book-authors">{ authors }</div>
       </div>
 
     )
